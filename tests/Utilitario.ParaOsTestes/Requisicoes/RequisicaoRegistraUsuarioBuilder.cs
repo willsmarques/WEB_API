88401cabdde0adfc22e0258ec44455866af804aa
William Marques <willsmarques@gmail.com>
2023-09-21 12:48:16 -0300

    criando testes de unidade para o validator de registrar usuario

---
+++ tests/Utilitario.ParaOsTestes/Requisicoes/RequisicaoRegistraUsuarioBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using MeuLivroDeReceitas.Comunicacao.Requisicoes;
+
+namespace Utilitario.ParaOsTestes.Requisicoes;
+
+public class RequisicaoRegistraUsuarioBuilder
+{
+    public static RequisicaoRegistrarUsuarioJson Contruir(int tamanhoSenha = 10)
+    {
+        return new Faker<RequisicaoRegistrarUsuarioJson>()
+            .RuleFor(c => c.Nome, f => f.Person.FullName)
+            .RuleFor(c => c.Email, f => f.Internet.Email())
+            .RuleFor(c => c.Senha, f => f.Internet.Password(tamanhoSenha))
+            .RuleFor(c => c.Telefone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min: 1, max: 9)}"));
+
+    }
+
+
+}
